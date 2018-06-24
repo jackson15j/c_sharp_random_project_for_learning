42a8f93a13369120d3caf1a088600f17104174e1
Craig Astill <craig.astill@gmail.com>
2018-06-24 17:00:20 +0100

    Followed the steps to add arg input & fibonacci sequence.

--- hello_world/Program.cs
+++ hello_world/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+  Following the dotnet CLI tutorials:
+
+  - https://docs.microsoft.com/en-us/dotnet/core/tutorials/using-with-xplat-cli
+ */
+
+using System;
 
 namespace hello_world
 {
@@ -6,7 +12,38 @@ namespace hello_world
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length > 0)
+            {
+                // Run via: `dotnet run -- <arg>`.
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
+
+            Console.WriteLine("Fibonacci Numbers 1-15:");
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine($"{i + 1}: {FibonacciNumber(i)}");
+            }
+        }
+
+        static int FibonacciNumber(int n)
+        {
+            int a = 0;
+            int b = 1;
+            int tmp;
+
+            for (int i = 0; i < n; i++)
+            {
+                tmp = a;
+                a = b;
+                b += tmp;
+            }
+
+            return a;
         }
     }
 }
